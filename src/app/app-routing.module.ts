cbd0baf51673bb14e396958f3af85be2b90f60e8
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2023-09-07 11:11:16 +0530

    Working with routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BalanceComponent } from './balance/balance.component';
+import { DepositComponent } from './deposit/deposit.component';
+import { WithdrawComponent } from './withdraw/withdraw.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'balance',
+    component: BalanceComponent
+  },
+  {
+    path:'deposit', component:DepositComponent
+  },
+  {
+    path:'withdraw',
+    component:WithdrawComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
