462c4e56c367eb54283cda34ced6afb1395a9fda
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2023-09-07 11:50:44 +0530

    Working with Activated routes and methods

--- src/app/deposit/deposit.component.ts
+++ src/app/deposit/deposit.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-deposit',
   templateUrl: './deposit.component.html',
   styleUrls: ['./deposit.component.css']
 })
-export class DepositComponent {
+export class DepositComponent implements OnInit {
+  constructor (private route: ActivatedRoute, private router: Router ) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  coins (){
+    this.router.navigate(['coins'], {relativeTo : this.route});
+  }
+  notes (){
+    this.router.navigate(['notes'], { relativeTo: this.route} );
+  }
 
 }
